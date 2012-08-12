450d341b8e7a4c16bca5265ca59b7dc0c5f8b942
Markis Taylor <m@rkis.cc>
2012-08-12 18:09:17 -0400

    created homepage

--- app.js
+++ app.js
@@ -3,31 +3,34 @@ var ejs = require('ejs');
 var app = module.exports = express.createServer(express.logger());
 
 app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.register('.html', ejs);
+    
     app.use(express.compress()); 
     app.use(express.bodyParser());
-
-    app.set('views', __dirname + '/views');
-    app.register('.html', ejs);
-    app.set('view engine', 'ejs');
-    
-    app.use(express.static(__dirname + '/public'));
-    
     app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
     app.use(express.favicon(__dirname + '/public/favicon.ico', {maxAge: 31557600000}));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    
     app.use(app.router);
 });
 
 app.get('/', function(req, res){
-    res.render('category.html', { locals : { title: 'Category Page' } });
+    res.render('index.html', { locals : { title: 'Home Page', cssFileName: 'index' } });
 });
 
-app.get('/spill/', function(req, res){
-    res.render('spill.html', { locals : { title: 'Spill Page' } });
+app.get('/category', function(req, res){
+    res.render('category.html', { locals : { title: 'Category Page', cssFileName: 'category' } });
 });
 
-app.get('/elina', function(req, res) {
-    res.render('spill.html', { locals : { title: 'Spill Page' } });
+app.get('/spill', function(req, res) {
+    res.render('spill.html', { locals : { title: 'Spill Page', cssFileName: 'spill' } });
 });
     
-app.listen(process.env.PORT);
+app.get('/product', function(req, res) {
+    res.render('product.html', { locals : { title: 'Product Page', cssFileName: 'product' } });
+});
 
+app.listen(process.env.PORT);
